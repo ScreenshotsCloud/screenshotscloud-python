2e7bc010014da40ec9da9527bbe96cdeb279a8ab
Chris Hutchinson <chris@brushd.com>
2017-06-05 15:22:49 -0400

    Fix testing

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ from screenshotscloud import ScreenshotsCloud
 def test_screenshotscloud():
 	screenshotscloud = ScreenshotsCloud('my-key-generated-at-screenshots-dot-cloud', 'mysecretstringgeneratedatscreenshotsdotcloud')
 	return screenshotscloud.screenshotUrl({
-	    "url": "maps.google.com"
+	    "url": "bbc.com/news"
 	})
 
 def test_answer():
-	assert test_screenshotscloud() == 'https://api.screenshots.cloud/v1/screenshot/my-key-generated-at-screenshots-dot-cloud/20bc07c6b9d6875ddce3d7b69dbacac3c0ca3c7a?url=maps.google.com'
+	assert test_screenshotscloud() == 'https://api.screenshots.cloud/v1/screenshot/my-key-generated-at-screenshots-dot-cloud/3fc4e5cd418052343e3dc61b2eaf17bec53adaeb?url=bbc.com%2Fnews'
